bab4fe0ef804a6ba990ed85363b9563a534f64d4
uyjoong kim <bohaesoju@gmail.com>
2023-05-25 12:41:13 +0900

    로그인 처리

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -10,8 +11,18 @@ export class UsersService {
       private usersRepository: Repository<Users>
   ){}
   getUser(){}
-  
+
   async postUsers(email: string, nickname: string, password: string){
     const user = await this.usersRepository.findOne( {where: { email }})
+    if (user) {
+      //이미 존재하는 유저
+      return
+    }
+    const hashedPassword = await bcrypt.hash(password, 12)
+    await this.usersRepository.save({
+      email,
+      nickname,
+      password: hashedPassword
+    })
   }
 }
