1e2cb25c956d5528e894e3f6b7ca1e647d8789f1
uyjoong kim <bohaesoju@gmail.com>
2023-05-25 10:54:57 +0900

    bcrypt 설치

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+      private usersRepository: Repository<Users>
+  ){}
   getUser(){}
-  postUsers(email: string, nickname: string, password: string){}
+  
+  async postUsers(email: string, nickname: string, password: string){
+    const user = await this.usersRepository.findOne( {where: { email }})
+  }
 }
