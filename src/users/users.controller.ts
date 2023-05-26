f8e64721f330e4cd0ae5cb4bffa9deb1175f0767
bohaesoju <bohaesoju@gmail.com>
2023-05-26 13:44:37 +0900

    TypeOrmModule 적용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/common/decorators/user.decorator';
 import { UserDto } from 'src/common/dto/user.dto';
@@ -10,38 +18,36 @@ import { UsersService } from './users.service';
 @ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {
-
-  }
-  @ApiResponse( {type: UserDto})
-  @ApiOperation( {summary: '내 정보 조회' })
+  constructor(private readonly usersService: UsersService) {}
+  @ApiResponse({ type: UserDto })
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@User() user) {
-    return user
+    return user;
   }
 
-  @ApiOperation( {summary: '회원가입' })
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   join(@Body() data: JoinRequestDto) {
-    this.usersService.postUsers(data.email, data.nickname, data.password)
+    this.usersService.postUsers(data.email, data.nickname, data.password);
   }
 
-  @ApiResponse( {
+  @ApiResponse({
     status: 200,
     description: '성공',
     type: UserDto,
   })
-  @ApiOperation( {summary: '로그인' })
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn(@User() user) {
-    return user
+    return user;
   }
 
-  @ApiOperation( {summary: '로그아웃' })
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
-    req.logOut()
-    res.clearCookie('connect.sid', { httpOnly: true })
-    res.send('ok')
+    req.logOut();
+    res.clearCookie('connect.sid', { httpOnly: true });
+    res.send('ok');
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Users } from 'src/entities/Users';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  getUser(){}
-  postUsers(email: string, nickname: string, password: string){}
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  getUser() {}
+
+  async postUsers(email: string, nickname: string, password: string) {
+    if (!email) {
+      return;
+    }
+    const user = await this.usersRepository.findOne({ where: { email } });
+    if (user) {
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    await this.usersRepository.save({
+      email,
+      nickname,
+      password: hashedPassword,
+    });
+  }
 }
